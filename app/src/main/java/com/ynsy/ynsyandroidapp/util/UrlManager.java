e299ba83dfafc68496eb107af0dd2b440a366581
kongxu08 <232090914@qq.com>
2019-01-22 16:15:30 +0800

    修改外网地址、兼容老手机图片资源

--- app/src/main/java/com/ynsy/ynsyandroidapp/util/UrlManager.java
+++ app/src/main/java/com/ynsy/ynsyandroidapp/util/UrlManager.java
@@ -1,11 +1,15 @@
 package com.ynsy.ynsyandroidapp.util;
 
 public class UrlManager {
-    private static String seeyonHost = "http://172.16.7.211";
+    private static String seeyonHost = "http://116.54.19.198:8086";
     public static String seeyonToken =seeyonHost+"/seeyon/rest/token/admin/cjwsjy123";
     public static String seeyonHostAuthentication =seeyonHost+"/seeyon/rest/orgMember/effective/loginNameAndPassword/";
 
-    private static String appHost = "http://172.16.7.209:8080";
+//    private static String appHost = "http://172.16.7.209:8080";
+//app应用服务器 原172.16.7.209:8080 对应172.16.7.210:8081
+    private static String appHost = "http://116.54.19.198:8081";
+
+
    //public static String appRemoteLoginUrl = Host+"/yn/a/login";
     public static String appRemoteHomePageUrl = appHost+"/";
 //    public static String getUserInfoUrl = Host+"/yn/a/commonApi/getUserInfo";
